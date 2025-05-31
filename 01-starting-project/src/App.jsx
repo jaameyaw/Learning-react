d5ca8654a09d64bf846711ec6ffad9705b598a55
jaameyaw <johnsonameyaw22@gmail.com>
2025-05-31 05:31:52 +0000

    setting html imgs attribute dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import reactImg from './assets/react-core-concepts.png';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -8,7 +9,7 @@ function Header () {
   const descriptions = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-      <img src= "src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src= {reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {descriptions} React concepts you will need for almost any app you are
