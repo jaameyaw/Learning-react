a81d6b1de26335e59f30186b1e2b014ff9cf168a
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-13 23:31:49 +0000

    change player name to input on button clicked

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,12 +1,20 @@
 
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false)
+    function isClicked() {
+        setIsEditing(true)
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {isEditing? <input type="text" />: <span className="player-name">{name}</span>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={isClicked}>Edit</button>
         </li>
     )
 }
