07896cb29905d3d2b75c6d830e4872be59fab66b
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-14 02:34:37 +0000

    handle player symbol button onclick

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialGameBoard = [
     [null, null, null],
@@ -7,14 +8,23 @@ const initialGameBoard = [
 
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handlePlayerSymbolBtn(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard
+        })
+    }
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => 
+            {gameBoard.map((row, rowIndex) => 
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => 
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handlePlayerSymbolBtn(rowIndex, colIndex) }>{playerSymbol}</button>
                             </li>
                         )}
                     </ol>
