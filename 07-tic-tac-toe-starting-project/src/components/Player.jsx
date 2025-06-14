36b3e1b4f1193919eb56f491611409c0502a4848
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-14 00:22:00 +0000

    handling player name input value

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,17 +1,22 @@
 
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
-
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
+
     function handleEditClick() {
         setIsEditing((prev) => !prev);
+    }
+
+    function handleInputValue(e) {
+        setPlayerName(e.target.value)
     }
 
     return (
         <li>
             <span className="player">
-              {isEditing? <input type="text" required value={name}/>: <span className="player-name">{name}</span>}
+              {isEditing? <input type="text" required value={playerName} onChange={handleInputValue} />: <span className="player-name">{playerName}</span>}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing? 'Save': 'Edit'}</button>
