428f74a3cfd09cf8e32e087c5b6fb38d85705156
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-17 20:41:53 +0000

    add highlight-player className

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
+        <ol id="players" className="highlight-player">
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
         </ol>
