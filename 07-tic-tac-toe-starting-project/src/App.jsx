38394a9ea243f710843438911e8943f8d0d7149d
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-25 14:48:02 +0000

    pass handlePlayerName fn to  onchangeName prop

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -85,13 +85,13 @@ function App() {
       [symbol]: newName
     }});
   } 
-  
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange}/>
         </ol>
         {(winner || hasDraw)  && 
         <GameOver winner={winner} onRematch={handleRestartGame}/>}
