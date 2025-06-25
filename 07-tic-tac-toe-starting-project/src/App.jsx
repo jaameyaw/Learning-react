d0d24a236780144a0679098a3e0ece04f78a13a5
jaameyaw <johnsonameyaw22@gmail.com>
2025-06-25 14:37:09 +0000

    make winning player name appear on winner modal

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -22,7 +22,12 @@ function derivedActivePlayer(gameTurns) {
 
 }
 
+
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+  })
   const [gameTurns, setGameTurns] = useState([])
   // const [activePlayer, setActivePlayer] = useState('X')
   
